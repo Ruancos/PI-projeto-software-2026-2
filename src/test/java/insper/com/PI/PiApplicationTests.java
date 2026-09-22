8a9c43db04f224d23968b3fb929d2aa7de2462b7
Ruan Cesar <ruancos@al.insper.edu.br>
2026-09-22 13:01:45 -0300

    initial commit

---
+++ src/test/java/insper/com/PI/PiApplicationTests.java
@@ -0,0 +1,13 @@
+package insper.com.PI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
